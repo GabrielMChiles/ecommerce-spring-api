fa44db5a3f0fabfce5e6c1448da36983b1aee513
Gabriel Machado <gabrielchiles.engenharia@gmail.com>
2026-03-31 20:10:21 -0300

    Criação de entidades iniciais

---
+++ src/main/java/com/senai/gabrielm/ecommerce_spring_api/model/Endereco.java
@@ -0,0 +1,4 @@
+package com.senai.gabrielm.ecommerce_spring_api.model;
+
+public class Endereco {
+}
---
+++ src/main/java/com/senai/gabrielm/ecommerce_spring_api/model/enums/status.java
@@ -0,0 +1,4 @@
+package com.senai.gabrielm.ecommerce_spring_api.model.enums;
+
+public enum status {
+}
