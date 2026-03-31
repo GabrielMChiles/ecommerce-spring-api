8d405b7f3f472471d3edd51317581c8c45fe2d6f
Gabriel Machado <gabrielchiles.engenharia@gmail.com>
2026-03-31 19:34:29 -0300

    Commit inicial

---
+++ src/main/java/com/senai/gabrielm/ecommerce_spring_api/EcommerceSpringApiApplication.java
@@ -0,0 +1,13 @@
+package com.senai.gabrielm.ecommerce_spring_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceSpringApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceSpringApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/senai/gabrielm/ecommerce_spring_api/EcommerceSpringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senai.gabrielm.ecommerce_spring_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceSpringApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
